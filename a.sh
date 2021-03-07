d1fcdcebf04d5f7f1fb4ef6442546ca1b05f8fbd
sushmi <sushmi.yogesh@gmail.com>
2021-03-07 20:43:04 +0530

    file changed now

--- a.sh
+++ a.sh
@@ -1,24 +1,25 @@
+#!/bin/sh
 echo "select the operation need to perform(add/sub/mul/div)"
-read oper
+read operation
 echo "provide the value for the variable a and b"
 read a b
-case $oper in
+case $operation in
 add)
     val=`expr $a + $b`  #To perform addition operation
-    echo -e "\e[1;31m $val \e[0m" #Print the values in red coloured format
+    echo -e " a+b=\e[1;31m $val \e[0m" #Print the values in red coloured format
     
     ;;
 sub)
     val=`expr $a - $b` #To perform subtract operation
-    echo -e "\e[1;33m $val \e[0m" #Print the values in yellow coloured format
+    echo -e "a-b=\e[1;33m $val \e[0m" #Print the values in yellow coloured format
     ;;
 mul)
    val=`expr $a \* $b` #To perform multiplication operation
-   echo -e "\e[1;34m $val \e[0m" #Print the values in blue coloured format
+   echo -e "a*b=\e[1;34m $val \e[0m" #Print the values in blue coloured format
    ;;
 div)
    val=`expr $a / $b` #To perform division operation
-   echo -e "\e[1;32m $val \e[0m" #Print the values in green coloured format
+   echo -e "a/b=\e[1;32m $val \e[0m" #Print the values in green coloured format
    ;;
 esac
 

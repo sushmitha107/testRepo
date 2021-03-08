a2bbee15189013a7827d995908e5ceae691c4011
sushmi <sushmi.yogesh@gmail.com>
2021-03-08 09:46:25 +0530

    fileaddchange

--- a.sh
+++ a.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
-echo "select the operation need to perform(add/sub/mul/div)"
-read operation
-echo "provide the value for the variable a and b"
-read a b
-case $operation in
-add)
+#Take user Input
+echo "Enter two numbers : "
+read a 
+read b
+#Input Type of Operation 
+echo "Enter Choice : "
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multplication"
+echo "4. Division"
+read ch
+#switch case to perform
+#calculator operations
+case $ch  in
+1)
     val=`expr $a + $b`  #To perform addition operation
     echo -e " a+b=\e[1;31m $val \e[0m" #Print the values in red coloured format
     
     ;;
-sub)
+2)
     val=`expr $a - $b` #To perform subtract operation
     echo -e "a-b=\e[1;33m $val \e[0m" #Print the values in yellow coloured format
     ;;
-mul)
+3)
    val=`expr $a \* $b` #To perform multiplication operation
    echo -e "a*b=\e[1;34m $val \e[0m" #Print the values in blue coloured format
    ;;
-div)
+4)
    val=`expr $a / $b` #To perform division operation
    echo -e "a/b=\e[1;32m $val \e[0m" #Print the values in green coloured format
    ;;
